2133f7e8d98726f51d4c52bbe11b56dfc1a47a58
Greg Schuman <gcschuman@hotmail.com>
2020-12-22 20:57:06 -0800

    Added deleteHandler function in Todo.js.

--- to-do-list/src/components/Todo.js
+++ to-do-list/src/components/Todo.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
 const Todo = ({ text }) => {
+    // Events
+    const deleteHandler = () => {
+        
+    }
     return(
         <div className="todo">
-            <li className="todo-item"></li>
+            <li className="todo-item">{text}</li>
             <button className="complete-button">
                 <i className="fas-fa-check"></i>
             </button>
