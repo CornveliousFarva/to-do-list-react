a308605c735c6434fa0c562c4dfc0d0d763660fe
Greg Schuman <gcschuman@hotmail.com>
2020-12-22 21:22:46 -0800

    Added a little bit of the delete handler function and fixed css issue with the complete and trash buttons.

--- to-do-list/src/components/Todo.js
+++ to-do-list/src/components/Todo.js
@@ -1,17 +1,17 @@
 import React from 'react'
 
-const Todo = ({ text, todos, setTodosa }) => {
+const Todo = ({ text, todo, todos, setTodos }) => {
     // Events
     const deleteHandler = () => {
-
+        setTodos(todo.filter((el) => el.id !== todo.id))
     }
     return(
         <div className="todo">
             <li className="todo-item">{text}</li>
-            <button className="complete-button">
+            <button className="complete-btn">
                 <i className="fas-fa-check"></i>
             </button>
-            <button className="trash-button">
+            <button onClick={deleteHandler} className="trash-btn">
                 <i className="fas-fa-trash"></i>
             </button>
         </div>
