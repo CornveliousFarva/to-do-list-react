41be39a0d2738f7f3fcd3edd69aa7a8b2cfcba8e
Greg Schuman <gcschuman@hotmail.com>
2020-12-26 10:52:01 -0800

    Added status and setStatus filter.

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 function App() {
   const [inputText, setInputText] = useState ("")
   const [todos, setTodos] = useState ([])
+  const [status, setStatus] = useState("all")
   return (
     <div className="App">
       <header>
